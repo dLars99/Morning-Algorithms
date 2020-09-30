97dded2ba1d0f7d0fc8c48349c15d72c71db1474
David Larsen <davidplarsen@gmail.com>
2020-09-30 09:14:45 -0500

    Recursive range solution

---
+++ recursiveRange/recursiveRange.js
@@ -0,0 +1,11 @@
+
+function recursiveRange(num) {
+    // 1. Establish a base case
+    if (num === 0) return 0
+    // 2. Recursive call/different number
+    return num + recursiveRange(num - 1)
+}
+
+// SAMPLE INPUT/OUTPUT
+console.log(recursiveRange(6)) // 21
+console.log(recursiveRange(10)) // 55 
